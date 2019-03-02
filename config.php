bde414e956257a126201e6a29110bca1a4fe1f99
gilangirchas <48150762+gilangirchas@users.noreply.github.com>
2019-03-02 09:31:00 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "xxxxxcxxx";
+//user_token wajib di isi //
+$user_token="xxxxxxxxx";
+
+//user_id wajib di isi //
+$user_id="xxxxxxxxxxx";
+
+// invite_code wajib di isi //
+$invite_code="xxxxxxxxxx";
+
+// Bearer wajib di isi //
+$Bearer="xxxxxxxxxxxxxxxx";
+
+
+
+?>
